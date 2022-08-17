a319395652b2983d525153bf3752d1852d84ccb4
Tri An Nguyen <triandn43@gmail.com>
2022-08-17 15:38:04 +0700

    commit v1

---
+++ core/admin.py
@@ -0,0 +1,6 @@
+from tokenize import Pointfloat
+from django.contrib import admin
+from .models import Profile
+# Register your models here.
+
+admin.site.register(Profile)
---
+++ core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def signup(request):
+    return render(request , 'signup.html')
